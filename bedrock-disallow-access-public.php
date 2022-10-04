803aa7ac028beeea06769405a2273987c88f5c3b
David <baller.david@outlook.fr>
2022-10-04 11:10:56 +0200

    feat: add hook template_redirect - redirect user if non-logged in

---
+++ bedrock-disallow-access-public.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name:  Disallow access public
+Plugin URI:   https://github.com/david-bzh/bedrock-disallow-access-public
+Description:  This plugin will prevent access to the site for non-logged in users when `WP_ENV` is set to `staging`.
+Version:      1.0.0
+Author:       David
+Text Domain:  plugin-disallow-access-public
+*/
+
+add_action(
+	'template_redirect',
+	function () {
+
+		if ( 'staging' === getenv( 'WP_ENV' ) && $_REQUEST['q'] !== 'wp-login.php' && ! is_user_logged_in() ) {
+
+			wp_safe_redirect( site_url() . '/wp-login.php' );
+		}
+	}
+);
